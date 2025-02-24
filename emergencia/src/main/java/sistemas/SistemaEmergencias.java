971a5d9609926f7f8b2f416cba0fcfb47109019b
eyderarryo19@gmail.com <eyderarroyo19@gmail.com>
2025-02-24 14:11:19 -0500

    Correccion clase SistemaEmer...

--- emergencia/src/main/java/sistemas/SistemaEmergencias.java
+++ emergencia/src/main/java/sistemas/SistemaEmergencias.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SistemaEmergencias {
-    private List<Emergencia> emergencias;
-    private Bomberos bomberos;
-    private Ambulancia ambulancia;
-    private Policia policia;
+    private final List<Emergencia> emergencias;
+    private final Bomberos bomberos;
+    private final Ambulancia ambulancia;
+    private final Policia policia;
 
     public SistemaEmergencias(Bomberos bomberos, Ambulancia ambulancia, Policia policia) {
         this.bomberos = bomberos;
@@ -25,20 +25,19 @@ public class SistemaEmergencias {
         ServicioEmergencia servicio = null;
 
         switch (emergencia.getTipo()) {
-            case "Incendio":
-                servicio = bomberos;
-                break;
-            case "Accidente Vehicular":
-                servicio = ambulancia;
-                break;
-            case "Robo":
-                servicio = policia;
-                break;
-            default:
+            case "Incendio" -> servicio = bomberos;
+            case "Accidente Vehicular" -> servicio = ambulancia;
+            case "Robo" -> servicio = policia;
+            default -> {
                 System.out.println("Tipo de emergencia no reconocido.");
                 return;
+            }
         }
 
+        extracted(emergencia, servicio);
+    }
+
+    private void extracted(Emergencia emergencia, ServicioEmergencia servicio) {
         servicio.atenderEmergencia(emergencia);
     }
 
@@ -48,5 +47,33 @@ public class SistemaEmergencias {
         System.out.println("Camiones de bomberos disponibles: " + bomberos.getCamionesDisponibles());
         System.out.println("Ambulancias disponibles: " + ambulancia.getAmbulanciasDisponibles());
         System.out.println("Unidades policiales disponibles: " + policia.getUnidadesDisponibles());
+    }
+
+    public List<Emergencia> getEmergencias() {
+        return emergencias;
+    }
+
+    public Bomberos getBomberos() {
+        return bomberos;
+    }
+
+    public Ambulancia getAmbulancia() {
+        return ambulancia;
+    }
+
+    public Policia getPolicia() {
+        return policia;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("SistemaEmergencias{");
+        sb.append("emergencias=").append(emergencias);
+        sb.append(", bomberos=").append(bomberos);
+        sb.append(", ambulancia=").append(ambulancia);
+        sb.append(", policia=").append(policia);
+        sb.append('}');
+        return sb.toString();
     }
 }
